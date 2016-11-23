a3f123de2dbc845f211833b99a575eed16b2c5b6
Sharanpreet Singh <Sharan-Lobana@users.noreply.github.com>
2016-11-23 12:44:21 +0530

    Added comments and proper indentation

--- user.h
+++ user.h
@@ -1,13 +1,13 @@
 void reverse(char *str, int len)
 {
-    int i=0, j=len-1, temp;
-    while (i<j)
-    {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-        i++; j--;
-    }
+  int i=0, j=len-1, temp;
+  while (i<j)
+  {
+    temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+    i++; j--;
+  }
 }
 
  // Converts a given integer x to string str[].  d is the number
@@ -15,47 +15,47 @@ void reverse(char *str, int len)
  // of digits in x, then 0s are added at the beginning.
 int intToStr(int x, char str[], int d)
 {
-    int i = 0;
-    while (x)
-    {
-        str[i++] = (x%10) + '0';
-        x = x/10;
-    }
- 
-    // If number of digits required is more, then
-    // add 0s at the beginning
-    while (i < d)
-        str[i++] = '0';
- 
-    reverse(str, i);
-    str[i] = '\0';
-    return i;
+  int i = 0;
+  while (x)
+  {
+    str[i++] = (x%10) + '0';
+    x = x/10;
+  }
+
+  // If number of digits required is more, then
+  // add 0s at the beginning
+  while (i < d)
+    str[i++] = '0';
+
+  reverse(str, i);
+  str[i] = '\0';
+  return i;
 }
- 
+
 // Converts a floating point number to string.
 void ftoa(float n, char *res, int afterpoint)
 {
-    // Extract integer part
-    int ipart = (int)n;
- 
-    // Extract floating part
-    float fpart = n - (float)ipart;
- 
-    // convert integer part to string
-    int i = intToStr(ipart, res, 0);
- 
-    // check for display option after point
-    if (afterpoint != 0)
-    {
-        res[i] = '.';  // add dot
- 
-        // Get the value of fraction part upto given no.
-        // of points after dot. The third parameter is needed
-        // to handle cases like 233.007
-        fpart = fpart * pow(10, afterpoint);
- 
-        intToStr((int)fpart, res + i + 1, afterpoint);
-    }
+  // Extract integer part
+  int ipart = (int)n;
+
+  // Extract floating part
+  float fpart = n - (float)ipart;
+
+  // convert integer part to string
+  int i = intToStr(ipart, res, 0);
+
+  // check for display option after point
+  if (afterpoint != 0)
+  {
+    res[i] = '.';  // add dot
+
+    // Get the value of fraction part upto given no.
+    // of points after dot. The third parameter is needed
+    // to handle cases like 233.007
+    fpart = fpart * pow(10, afterpoint);
+
+    intToStr((int)fpart, res + i + 1, afterpoint);
+  }
 }
 
 void specialKeys(int key, int x, int y)
@@ -91,7 +91,7 @@ void specialKeys(int key, int x, int y)
 
 void rotate()
 {
-	double acc = progstep * cos(wind_x/180*PI)*cos(wind_y/180*PI) * wind_acc_factor - wing_speed * turbine_factor;
+	double acc = progstep*cos(wind_y/180*PI)*wind_acc_factor - wing_speed*turbine_factor;
 
 	wing_speed += acc;
 	wing_z += wing_speed;
@@ -117,8 +117,8 @@ void mouseWheel(int button, int dir, int x, int y)
 {
     if (button == 3)
     {
-	// Zoom in
-	if(scale <= 3.0)
+	     // Zoom in
+	      if(scale <= 3.0)
 		scale += 0.03;
     }
     else if(button == 4)
